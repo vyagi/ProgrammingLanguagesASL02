e628c9fc4cf25c0bce9dae4d40318ac440fa3459
Marcin Jagieła <marcin.jagiela@hotmail.com>
2020-10-16 19:40:59 +0200

    Initial commit

---
+++ BasicLanguageFeatures/BasicStructures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
