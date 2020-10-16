c9f19c0873653ecae362eca4d56a908b44b64a60
Marcin Jagieła <marcin.jagiela@hotmail.com>
2020-10-16 19:41:56 +0200

    Message improved

--- BasicLanguageFeatures/BasicStructures/Program.cs
+++ BasicLanguageFeatures/BasicStructures/Program.cs
@@ -6,7 +6,7 @@ namespace BasicStructures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World from the Visual Studio!");
         }
     }
 }
