d75213cfabac5b19379c1e6983656a5ed0964032
Marcin Jagieła <marcin.jagiela@hotmail.com>
2020-12-02 17:18:43 +0100

    Invoice class ready

--- BasicLanguageFeatures/InvoiceManager/Form1.Designer.cs
+++ BasicLanguageFeatures/InvoiceManager/Form1.Designer.cs
@@ -29,21 +29,22 @@ namespace InvoiceManager
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.readButton = new System.Windows.Forms.Button();
             this.pathTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.totalByNamesButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // readButton
             // 
-            this.button1.Location = new System.Drawing.Point(12, 59);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Read file";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.readButton.Location = new System.Drawing.Point(12, 59);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(99, 23);
+            this.readButton.TabIndex = 0;
+            this.readButton.Text = "Read file";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.button1_Click);
             // 
             // pathTextBox
             // 
@@ -71,15 +72,26 @@ namespace InvoiceManager
             this.resultTextBox.Size = new System.Drawing.Size(775, 339);
             this.resultTextBox.TabIndex = 3;
             // 
+            // totalByNamesButton
+            // 
+            this.totalByNamesButton.Location = new System.Drawing.Point(118, 60);
+            this.totalByNamesButton.Name = "totalByNamesButton";
+            this.totalByNamesButton.Size = new System.Drawing.Size(90, 23);
+            this.totalByNamesButton.TabIndex = 4;
+            this.totalByNamesButton.Text = "Total by names";
+            this.totalByNamesButton.UseVisualStyleBackColor = true;
+            this.totalByNamesButton.Click += new System.EventHandler(this.totalByNamesButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.totalByNamesButton);
             this.Controls.Add(this.resultTextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pathTextBox);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.readButton);
             this.Name = "Form1";
             this.Text = "Invoice manager";
             this.ResumeLayout(false);
@@ -89,10 +101,11 @@ namespace InvoiceManager
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button readButton;
         private System.Windows.Forms.TextBox pathTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Button totalByNamesButton;
     }
 }
 
--- BasicLanguageFeatures/InvoiceManager/Form1.cs
+++ BasicLanguageFeatures/InvoiceManager/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace InvoiceManager
@@ -19,6 +20,34 @@ namespace InvoiceManager
             }
 
             resultTextBox.Text = File.ReadAllText(path);
+        }
+
+        private void totalByNamesButton_Click(object sender, EventArgs e)
+        {
+            var path = pathTextBox.Text;
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File does not exist. Cannot proceed");
+                return;
+            }
+
+            var lines = File.ReadAllLines(path).Skip(1);
+
+        }
+    }
+
+    public class Invoice
+    {
+        public string Name { get; }
+        public DateTime DateTime { get; }
+        public decimal Amount { get; }
+
+        public Invoice(string name, DateTime dateTime, decimal amount)
+        {
+            Name = name;
+            DateTime = dateTime;
+            Amount = amount;
         }
     }
 }
