8e832e33ac3a19fa874b221fa0efd0b70f3eb2dd
Marcin Jagieła <marcin.jagiela@hotmail.com>
2020-12-09 16:09:31 +0100

    Beginning of testing

---
+++ TDD/Geometry.Tests/PointTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Geometry.Tests
+{
+    public class PointTests
+    {
+        [Fact]
+        public void Can_create_a_point_with_two_arguments()
+        {
+            //Arrange
+            var point = new Point(1,3);
+
+            //Act
+
+            //Assert
+        }
+    }
+}
---
+++ TDD/Geometry/Point.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry
+{
+    public class Point
+    {
+        public Point(double x, double y)
+        {
+            
+        }
+    }
+}
